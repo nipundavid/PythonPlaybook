031b2386cab1c7a2736686a9e615c824d3c88268
Nipun David <nipun18david@gmail.com>
2021-10-02 15:12:40 +0530

    color detection added

---
+++ OpenCV/GettingStarted/color_detection.py
@@ -0,0 +1,95 @@
+import cv2
+import numpy as np
+
+'''We convert the image to HSV space, because You can usually get better information from a HSV colorspace. The 
+reason we use HSV colorspace for color detection/thresholding over RGB/BGR is that HSV is more robust towards 
+external lighting changes. This means that in cases of minor changes in external lighting (such as pale shadows, 
+etc. ) Hue values vary relatively lesser than RGB values. For example, two shades of red colour might have similar 
+Hue values, but widely different RGB values. In real life scenarios such as object tracking based on colour,
+we need to make sure that our program runs well irrespective of environmental changes as much as possible. So, 
+we prefer HSV colour thresholding over RGB'''
+
+
+
+'''
+In CV we have hue max of 180 i.e. 0-179, not 360 like, 
+Saturation is from 0-255
+Value is from 0-255
+'''
+
+def empty(a):
+    pass
+
+
+def stackImages(scale, imgArray):
+    rows = len(imgArray)
+    cols = len(imgArray[0])
+    rowsAvailable = isinstance(imgArray[0], list)
+    width = imgArray[0][0].shape[1]
+    height = imgArray[0][0].shape[0]
+    if rowsAvailable:
+        for x in range(0, rows):
+            for y in range(0, cols):
+                if imgArray[x][y].shape[:2] == imgArray[0][0].shape[:2]:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (0, 0), None, scale, scale)
+                else:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (imgArray[0][0].shape[1], imgArray[0][0].shape[0]),
+                                                None, scale, scale)
+                if len(imgArray[x][y].shape) == 2: imgArray[x][y] = cv2.cvtColor(imgArray[x][y], cv2.COLOR_GRAY2BGR)
+        imageBlank = np.zeros((height, width, 3), np.uint8)
+        hor = [imageBlank] * rows
+        hor_con = [imageBlank] * rows
+        for x in range(0, rows):
+            hor[x] = np.hstack(imgArray[x])
+        ver = np.vstack(hor)
+    else:
+        for x in range(0, rows):
+            if imgArray[x].shape[:2] == imgArray[0].shape[:2]:
+                imgArray[x] = cv2.resize(imgArray[x], (0, 0), None, scale, scale)
+            else:
+                imgArray[x] = cv2.resize(imgArray[x], (imgArray[0].shape[1], imgArray[0].shape[0]), None, scale, scale)
+            if len(imgArray[x].shape) == 2: imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
+        hor = np.hstack(imgArray)
+        ver = hor
+    return ver
+
+path = 'Resources/lambo.png'
+cv2.namedWindow("TrackBars")
+cv2.resizeWindow("TrackBars", 640, 240)
+cv2.createTrackbar("Hue Min", "TrackBars", 0, 179, empty)
+cv2.createTrackbar("Hue Max", "TrackBars", 19, 179, empty)
+cv2.createTrackbar("Sat Min", "TrackBars", 110, 255, empty)
+cv2.createTrackbar("Sat Max", "TrackBars", 240, 255, empty)
+cv2.createTrackbar("Val Min", "TrackBars", 153, 255, empty)
+cv2.createTrackbar("Val Max", "TrackBars", 255, 255, empty)
+
+while True:
+    img = cv2.imread(path)
+    imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    h_min = cv2.getTrackbarPos("Hue Min", "TrackBars")
+    h_max = cv2.getTrackbarPos("Hue Max", "TrackBars")
+    s_min = cv2.getTrackbarPos("Sat Min", "TrackBars")
+    s_max = cv2.getTrackbarPos("Sat Max", "TrackBars")
+    v_min = cv2.getTrackbarPos("Val Min", "TrackBars")
+    v_max = cv2.getTrackbarPos("Val Max", "TrackBars")
+
+    print(h_min, h_max, s_min, s_max, v_min, v_max)
+
+    lower = np.array([h_min, s_min, v_min])
+    upper = np.array([h_max, s_max, v_max])
+
+    mask = cv2.inRange(imgHSV, lowerb=lower, upperb=upper)
+    '''Find the intersection of the image and mask'''
+    imageResult = cv2.bitwise_and(img, img, mask=mask)
+
+    # cv2.imshow("Original", img)
+    # cv2.imshow("HSV Image", imgHSV)
+    # cv2.imshow("Mask", mask)
+    # cv2.imshow("Result", imageResult)
+    
+    imgStack = stackImages(0.6, ([img, imgHSV], [mask, imageResult]))
+    cv2.imshow("Detect Color", imgStack)
+
+    cv2.waitKey(1)
+
+
